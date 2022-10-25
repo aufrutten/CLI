2717a7cf66e4be46be0d2c52e8367b91f036c140
Semykopenko Ihor <aufrutten@gmail.com>
2022-10-25 21:52:30 +0200

    adding parse functions

--- tests/test_collect_framework.py
+++ tests/test_collect_framework.py
@@ -2,12 +2,17 @@ import pytest
 import collect_framework
 
 
-def test_counting_unique_characters():
-    assert collect_framework.counting_unique_characters('abbbccdf') == 3
-    assert collect_framework.counting_unique_characters('aabcdfee') == 4
+class TestCountingUniqueCharacters:
+    """test class for testing func: 'counting_unique_characters' """
+
+    def test_counting_unique_characters(self):
+        assert collect_framework.counting_unique_characters('abbbccdf') == 3
+        assert collect_framework.counting_unique_characters('aabcdfee') == 4
+
+    def test_counting_unique_characters_assertion(self):
+        with pytest.raises(TypeError):
+            collect_framework.counting_unique_characters(123)
 
 
-def test_counting_unique_characters_assertion():
-    with pytest.raises(TypeError):
-        collect_framework.counting_unique_characters(123)
-
+class TestParseCLI:
+    pass
