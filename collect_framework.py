f639724c7a9e742d773131b44a964b5fa4c96006
Semykopenko Ihor <aufrutten@gmail.com>
2022-10-27 08:52:26 +0200

    save for refactoring

--- collect_framework.py
+++ collect_framework.py
@@ -4,36 +4,40 @@ import argparse
 import os
 
 
-def parse_file(file):
-    """function for parse files, checks if path exists and that is file"""
-    if os.path.isfile(file):
-        with open(file, 'r') as file:
-            return file.read()
-    else:
-        raise FileNotFoundError("file doesn't exist or that is folder")
+class Parser:
 
+    def __init__(self):
+        """init function of parser, adding arguments"""
+        self.parser_cli = argparse.ArgumentParser(description="Counter of unique characters")
+        self.parser_cli.add_argument("--file",
+                                     type=str,
+                                     help="file for counting")  # add --file argument
 
-def parse_cli_function():
-    """
-    function for parse our command line
-    function have three priority of return, that is: 1) file 2) text 3) nothing
-    function return text
-    """
-    parser_cli = argparse.ArgumentParser(description="Counter of unique characters")
-    parser_cli.add_argument("--file",
-                            type=str,
-                            help="file for counting")  # add --file argument
+        self.parser_cli.add_argument("--string",
+                                     type=str,
+                                     help="string for counting")  # add --sting argument
 
-    parser_cli.add_argument("--string",
-                            type=str,
-                            help="string for counting")  # add --sting argument
+    def parse_cli_function(self):
+        """
+        function for parse our command line
+        function have three priority of return, that is: 1) file 2) text 3) nothing
+        function return text
+        """
+        cli_args = self.parser_cli.parse_args()  # getting all arguments in CLI
+        if cli_args.file is not None:
+            return self.parse_file(file=cli_args.file)
 
-    cli_args = parser_cli.parse_args()  # getting all arguments in CLI
-    if cli_args.file is not None:
-        return parse_file(file=cli_args.file)
+        elif cli_args.string is not None:
+            return cli_args.string
 
-    elif cli_args.string is not None:
-        return cli_args.string
+    @staticmethod
+    def parse_file(file):
+        """function for parse files, checks if path exists and that is file"""
+        if os.path.isfile(file):
+            with open(file, 'r') as file:
+                return file.read()
+        else:
+            raise FileNotFoundError("file doesn't exist or that is folder")
 
 
 @lru_cache(maxsize=None)
@@ -46,7 +50,7 @@ def counting_unique_characters(text: str) -> int:
 
 
 def main():  # pragma: no cover
-    argument_of_cli = parse_cli_function()
+    argument_of_cli = Parser().parse_cli_function()
     counter = counting_unique_characters(argument_of_cli)
     response = f'{argument_of_cli}: {counter}'
     print(response)
--- tests/test_collect_framework.py
+++ tests/test_collect_framework.py
@@ -1,45 +1,46 @@
 import os
+import unittest
 from unittest.mock import Mock
 import pytest
 import collect_framework
 
 
-class TestCountingUniqueCharacters:
+class TestCountingUniqueCharacters(unittest.TestCase):
     """test class for testing func: 'counting_unique_characters' """
 
     def test_counting_unique_characters(self):
-        assert collect_framework.counting_unique_characters('abbbccdf') == 3
-        assert collect_framework.counting_unique_characters('aabcdfee') == 4
+        self.assertEqual(collect_framework.counting_unique_characters('abbbccdf'), 3)
+        self.assertEqual(collect_framework.counting_unique_characters('aabcdfee'), 4)
 
     def test_counting_unique_characters_assertion(self):
-        with pytest.raises(TypeError):
-            collect_framework.counting_unique_characters(123)
+        self.assertRaises(TypeError, collect_framework.counting_unique_characters, 123)
 
 
-class TestParseCLI:
+class TestParseCLI(unittest.TestCase):
     """test class  for testing func: 'parse_file' """
+
     def test_function_for_parse_file(self):
         """function tests 'parse_file', any case which have"""
+        # Parse.parse_file()
 
         # creating file with someone text
         path_to_temp_file = 'tempText.txt'
-        text_of_file_to_write = """Hello, my name is Ihor"""
+        text_of_file_to_write = """Hello, my name is Test"""
         with open(path_to_temp_file, 'w') as temporary_file:
             temporary_file.write(text_of_file_to_write)
             temporary_file.close()
 
         # trying to access a existent file
-        assert collect_framework.parse_file(path_to_temp_file) == text_of_file_to_write,\
-            'input text and file text do not match'
+        self.assertEqual(collect_framework.Parser.parse_file(path_to_temp_file), text_of_file_to_write,
+                         msg='input text and file text do not match')
 
         # trying to access a non-existent file
-        with pytest.raises(FileNotFoundError):
-            name_file_which_not_exist = 'non_existentText.txt'
-            collect_framework.parse_file(name_file_which_not_exist)
+        name_file_which_not_exist = 'non_existentText.txt'
+        self.assertRaises(FileNotFoundError, collect_framework.Parser.parse_file, name_file_which_not_exist)
 
         # deleting temp file after test
         os.remove(path_to_temp_file)
-        assert not os.path.isfile(path_to_temp_file), "file hasn't deleted"
+        self.assertFalse(os.path.isfile(path_to_temp_file), "file hasn't deleted")
 
     def test_parse_cli_function(self):
         """test function of 'parse_cli_function' """
